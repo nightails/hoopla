5b4cf76cb97bc6e14ac4801b7b638fdbb9e7e75d
Alan Night <a.nightails@gmail.com>
2026-05-06 13:28:28 -0500

    Add "build" command to CLI for building and saving inverted index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,14 @@
 import argparse
 import json
+import sys
 
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0,str(PROJECT_ROOT))
+
+from index import InvertedIndex
 from text import process_text
 
 
@@ -11,6 +19,8 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    build_parser = subparsers.add_parser("build", help="Build and save inverted index")
+
     args = parser.parse_args()
 
     match args.command:
@@ -19,6 +29,8 @@ def main() -> None:
             movies = movie_search(args.query)
             for movie in movies:
                 print(movie["title"])
+        case "build":
+            build_index()
         case _:
             parser.print_help()
 
@@ -37,6 +49,17 @@ def movie_search(keyword) -> list[dict]:
             movies_list.append(movie)
 
     return movies_list[:5]
+
+def build_index():
+    index = InvertedIndex()
+    index.build()
+    index.save()
+
+    docs = index.get_documents('merida')
+    if docs:
+        print(f"First document for token 'merida' = {docs[0]}")
+    else:
+        print("No documents found for token 'merida'")
 
 if __name__ == "__main__":
     main()
--- index/index.py
+++ index/index.py
@@ -2,7 +2,7 @@ import json
 import os
 import pickle
 
-from cli.text import process_text
+from text import process_text
 
 
 class InvertedIndex:

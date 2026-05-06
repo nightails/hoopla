9dfa15f7a78b4f38088706661fc7fd87185b5b3a
Alan Night <a.nightails@gmail.com>
2026-05-06 13:06:59 -0500

    Enhance InvertedIndex with JSON data loading, document mapping, and serialization

--- cli/index/index.py
+++ cli/index/index.py
@@ -1,13 +1,17 @@
+import json
+import os
+import pickle
+
 from cli.text import process_text
 
 
 class InvertedIndex:
     def __init__(self):
         self.index: dict[str, set[int]] = {}
-        self.docmap: dict[int, str] = {}
+        self.docmap: dict[int, dict] = {}
 
-    def __add_document(self, doc_id: int, content: str):
-        content_tokens = process_text(content)
+    def __add_document(self, doc_id: int, text: str):
+        content_tokens = process_text(text)
         for token in content_tokens:
             self.index[token] = self.index.get(token, set()).union({doc_id})
 
@@ -17,4 +21,14 @@ class InvertedIndex:
         return sorted(doc_ids)
 
     def build(self):
-        pass
+        with open("data/movies.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+        movies = data["movies"]
+        for m in movies:
+            self.__add_document(m["id"], f"{m['title']} {m['description']}")
+            self.docmap[m["id"]] = m
+
+    def save(self):
+        os.makedirs("cache", exist_ok=True)
+        pickle.dump(self.index, open("cache/index.pkl", "wb"))
+        pickle.dump(self.docmap, open("cache/docmap.pkl", "wb"))

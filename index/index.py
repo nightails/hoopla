f1f845704547a9a27d12c4aebbd0922b1e7570e1
Alan Night <a.nightails@gmail.com>
2026-05-06 14:18:21 -0500

    Add `get_tf` method to InvertedIndex for term frequency retrieval and integrate tokenization

--- index/index.py
+++ index/index.py
@@ -4,6 +4,7 @@ import os
 import pickle
 
 from text import process_text
+from text.sanitize_text import tokenize_string
 
 
 class InvertedIndex:
@@ -23,9 +24,16 @@ class InvertedIndex:
         term = term.lower()
         doc_ids = self.index.get(term, set())
         docs = []
-        for id in doc_ids:
-            docs.append(self.docmap[id])
+        for i in doc_ids:
+            docs.append(self.docmap[i])
         return sorted(docs, key=lambda x: x['id'])
+
+    def get_tf(self, doc_id, term) -> int:
+        term = term.lower()
+        term = tokenize_string(term)
+        if len(term) > 1:
+            raise ValueError("Term must be a single token")
+        return self.term_frequencies.get(doc_id, collections.Counter()).get(term, 0)
 
     def build(self):
         with open("data/movies.json", "r", encoding="utf-8") as file:

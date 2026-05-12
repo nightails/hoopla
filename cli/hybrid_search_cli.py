93d239779907939f400ef031f73ffa83bf73d468
Alan Night <a.nightails@gmail.com>
2026-05-12 14:23:50 -0500

    Add normalize function
    - util function to normalize a list float into range of 0.0 - 1.0

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,13 +1,24 @@
 import argparse
 
+from lib.hybrid_search import normalize_scores
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
-    parser.add_subparsers(dest="command", help="Available commands")
+    subparser = parser.add_subparsers(dest="command", help="Available commands")
+
+    normalize_parser = subparser.add_parser("normalize", help="Normalize numbers")
+    normalize_parser.add_argument("numbers", type=float, nargs="*", help="Numbers to normalize")
 
     args = parser.parse_args()
 
     match args.command:
+        case "normalize":
+            scores = normalize_scores(args.numbers)
+            if scores is None:
+                return
+            for score in scores:
+                print(f"* {score:0.4f}")
         case _:
             parser.print_help()
 
